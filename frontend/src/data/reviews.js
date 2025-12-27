e3737de20907a381cf219d11fe3d42475a259e2e
mdmitkar <mitkarmuhammad@gmail.com>
2025-12-27 23:53:25 +0530

    added some data

---
+++ frontend/src/data/reviews.js
@@ -0,0 +1,163 @@
+export const googleReviews = [
+    {
+        name: "Zeba Patel",
+        rating: 5,
+        review: "My child was very close to us, but when he started school he would cry a lot when he couldn't see us. However, alhamdulillah within two weeks he adjusted to school and now he is attached to it. He no longer cries, instead he is happy and even enjoys studying. Alhamdulillah, The school and education are excellent, and the activities conducted for the kids are great. Renaissance Preschool is doing a wonderful job. May Allah bless you with further progress and elevate the reputation of your school.",
+        date: "Edited a week ago",
+        response: "Thanks a lot sir for your kind words.."
+    },
+    {
+        name: "Tasdique Khan",
+        rating: 5,
+        review: "My daughter has had a wonderful experience at Renaissance School. The teachers are caring, patient, and provide a nurturing environment for learning. The school maintains excellent communication with the parents and organizes engaging activities that help children grow both academically and socially throughout.",
+        date: "Edited a week ago",
+        response: "Alhumdulillah, thanks for entrusting us, your encouragement is our strength."
+    },
+    {
+        name: "Ayesha Shaikh",
+        rating: 5,
+        review: "We are happy with our experience at Renaissance Preschool. The staff is caring and the teaching is clear. What makes it special for us is that the school also includes Islamic subjects along with academics, which helps children grow with good values.",
+        date: "Edited a week ago",
+        response: "Alhumdulillah, thanks for entrusting us, your encouragement is our strength."
+    },
+    {
+        name: "Uzma 4u",
+        rating: 5,
+        review: "I am happy with Renaissance school..I truly appreciate the way the school focuses on overall development . academic, decipline,values and extra curricular activities..my child enjoys coming to school every day, and I can already see the positive growth...All the staff members are very kind..😊",
+        date: "3 months ago",
+        response: "Thanks a lot Ma'am for your kind words........"
+    },
+    {
+        name: "Kaikashan Ansari",
+        rating: 5,
+        review: "Our experience with Renaissance Preschool has been excellent. The school not only makes learning enjoyable through creative activities but also follows a thoughtful evaluation pattern. I am impressed with how they assess children.",
+        date: "3 months ago",
+        response: "Thnx a ton for your appreciation, such words are the driving force for Team Renaissance....."
+    },
+    {
+        name: "Saher Jairumi",
+        rating: 5,
+        review: "My kid is extremely attached to his parents and have separation anxiety if his parents are not seen. But here Alhamdulillaah he adjusted so well within 20 days . Kudos to the hard work of team for understanding my child so well.",
+        date: "3 months ago",
+        response: "Thanks a lot Ma'am for your kind words........"
+    },
+    {
+        name: "Sana Nachan",
+        rating: 5,
+        review: "I recently had the opportunity to visit this school and I’m impressed with the warm and welcoming environment they’ve created. Even though the preschool has just opened, it already shows great potential.",
+        date: "7 months ago",
+        response: "Thnx a ton for your appreciation Ma'am, such words are the driving force for the team RENAISSANCE...."
+    },
+    {
+        name: "Sinin Mirekar",
+        rating: 5,
+        review: "Arabic acknowledgment is excellent ,Ali's confidence has been grown.Looking forward for betterment of my child and school as well.",
+        date: "3 months ago",
+        response: "Thnx a ton for your appreciation, such words are the driving force for us...."
+    },
+    {
+        name: "Ansari Sohaib",
+        rating: 5,
+        review: "One of the best pre school with extremely qualified teachers is now in operation in Bhiwandi. Please do visit for inquiries and new admissions.....",
+        date: "7 months ago",
+        response: "Thanx a ton sir..."
+    },
+    {
+        name: "Shagufta Madoo",
+        rating: 5,
+        review: "Went there. Was impressed . You would hardly find any preschool as well equipped as renaissance in bhiwandi.",
+        date: "7 months ago",
+        response: "Alhumdulillah, thanks for entrusting us, your encouragement is our strength."
+    },
+    {
+        name: "Meraj Ansari",
+        rating: 5,
+        review: "Highly recommend renaissance preschool for your kids.",
+        date: "7 months ago",
+        response: "Thanks a lot sir.."
+    },
+    {
+        name: "A. Patel",
+        rating: 5,
+        review: "Best and first step for early learning of child education.",
+        date: "7 months ago",
+        response: "thanks sir"
+    },
+    {
+        name: "Fareha Momin",
+        rating: 5,
+        review: "This is a right place for the preschoolers.children will learn with playing.",
+        date: "7 months ago",
+        response: "Thanks, your encouragement is our strength."
+    },
+    {
+        name: "Ayesha Shaikh",
+        rating: 5,
+        review: "Experienced team members.",
+        date: "7 months ago",
+        response: "Alhumdulillah"
+    },
+    {
+        name: "Arshad Bhati",
+        rating: 5,
+        review: "Nice School",
+        date: "7 months ago",
+        response: "Thanks"
+    },
+    {
+        name: "Fahad Dalvi",
+        rating: 5,
+        review: "For better education and worldly education, enroll your children in Renaissance School.",
+        date: "7 months ago",
+        response: "Thanks a lot sir.."
+    },
+    {
+        name: "Aamir Qureshi",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Alhumdulillah"
+    },
+    {
+        name: "Moazzib Mirekar",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Alhumdulillah"
+    },
+    {
+        name: "Minhaj Patel",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Alhumdulillah"
+    },
+    {
+        name: "Samira Mitkar",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Appreciated"
+    },
+    {
+        name: "Muhammad Mitkar",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Thanks"
+    },
+    {
+        name: "Sailor Sailor",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Thanks"
+    },
+    {
+        name: "Maria Rafi",
+        rating: 5,
+        review: "",
+        date: "7 months ago",
+        response: "Thanks"
+    }
+];
