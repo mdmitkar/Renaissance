08636b5176567aad63dbdd3b6a9656c56bcf86e5
MohammadM01 <mullamohammad123@gmail.com>
2026-01-02 17:29:46 +0530

    Home fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -535,7 +535,7 @@ const Home = () => {
             <section className="journey-section h-auto bg-gulf-lebanese text-white overflow-hidden relative flex flex-col md:flex-row items-center py-20 md:py-24">
 
                 {/* Background Text/Decor */}
-                <div className="absolute top-[10%] left-10 z-10">
+                <div className="absolute top-[15%] left-10 z-10">
                     <h2 className="text-xl font-bold uppercase tracking-[0.3em] text-white/30">
                         The Renaissance Journey
                     </h2>
