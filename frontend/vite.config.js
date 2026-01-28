86f9187ae6d49a8be0a007f39fd677cb479fe180
mdmitkar <mitkarmuhammad@gmail.com>
2026-01-28 14:22:01 +0530

    deploymebnt ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,32 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const pkg = require('vite-plugin-prerender');
-const prerender = pkg.default || pkg;
-const PuppeteerRenderer = pkg.PuppeteerRenderer;
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: [
-        '/',
-        '/about',
-        '/life',
-        '/reviews',
-        '/gallery',
-        '/inside-renaissance',
-        '/admission',
-        '/contact'
-      ],
-      renderer: new PuppeteerRenderer({
-        renderAfterElementExists: '#root div',
-        headless: true,
-      }),
-    }),
+    react()
   ],
 })
